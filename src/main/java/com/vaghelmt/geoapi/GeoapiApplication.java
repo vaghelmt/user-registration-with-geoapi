bc5b384dfe76769f70b2877a392cd76adf7845e7
Mitul Vaghela <mitul.vaghela@gmail.com>
2022-01-17 11:21:06 -0500

    initialized spring boot project

---
+++ src/main/java/com/vaghelmt/geoapi/GeoapiApplication.java
@@ -0,0 +1,13 @@
+package com.vaghelmt.geoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaghelmt/geoapi/GeoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaghelmt.geoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
